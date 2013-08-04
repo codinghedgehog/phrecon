048bfaa9b4faf8899dd6577fd7e041857723413e
codinghedgehog <github.20.cello@mapspart.com>
2013-08-04 14:53:31 -0500

    Version 3.1 of phrecon.py - Reverted to dictionary imple.
    Reverted from database driven to in-memory dictionary-based processing.

--- phrecon.py
+++ phrecon.py
@@ -27,19 +27,21 @@
 # Usage: phrecon.py <reference base file> <SNP loci input file> [-debug] [-quiet]
 #
 # 8/2/2013 - Version 3 - Andrew Pann, Ported version 2.1 of Perl-based Phrecon to Python, using Biopython and Sqlite3.
+# 8/4/2013 - Version 3.1 - Andrew Pann, Added multiprocessing to leverage multi-CPU performance.
 
 import argparse
 import os
 import sys
 import re
-import sqlite3
 import Bio
 import Bio.Seq
 import Bio.SeqIO
 import Bio.SeqRecord
 import Bio.Alphabet
+import multiprocessing
+import collections
 
-VERSION = "3.0"
+VERSION = "3.1"
 
 #############
 # FUNCTIONS
@@ -55,7 +57,7 @@ def print_debug(msg):
     if debug:
         if not quiet:
             print msg
-        logfile.write(msg)
+        logfile.write("{}\n".format(msg))
 
 def print_all(msg):
     '''Prints output to STDOUT and logfile in one call.
@@ -66,22 +68,7 @@ def print_all(msg):
     '''
     if not quiet:
         print msg
-    logfile.write(msg)
-
-def write_fasta(strainid,sequenceString):
-    '''Writes out the FASTA formatted entry for the strainid
-       with the given sequence String to the outfile
-    '''
-
-    outfile.write("\n")
-    outfile.write(">{}\n".format(strainid))
-    # Chop up the sequence to 60 characters per line.
-    fastaRow = [sequenceString[i:i+60] for i in range(0,len(sequenceString),60) ]
-    for fastaLine in fastaRow:
-        outfile.write("{}\n".format(fastaLine))
-    
-
-
+    logfile.write("{}\n".format(msg))
 
 
 ########################
@@ -97,7 +84,6 @@ parser.add_argument('reffilename', help="Reference base file name with line form
 parser.add_argument('snpfilename', help="SNP data file name with line format [STRAIN_ID] [LOCUS] [BASE]")
 parser.add_argument('-debug','--debug', action="store_true", help="Enable debugging output")
 parser.add_argument('-quiet','--quiet', action="store_true", help="Run in quiet mode (suppress most screen output)")
-parser.add_argument('-dbfile','--dbfile', type=str, nargs=1, help="Working database name (default is to load into memory)")
 args = parser.parse_args()
 
 reffile = open(args.reffilename,"r")
@@ -117,38 +103,8 @@ if debug:
 if quiet:
     print "Producing quiet (no stdout) output.  Logfile is still generated.\n"
 
-print_debug("Detected Biopython version {}\n".format(Bio.__version__))
 
-# Setup in-memory (or disk, if --dbfile is used) database.
-if args.dbfile != None:
-    dbfilename = args.dbfile[0]
-    if os.path.isfile(dbfilename):
-        print "*** ERROR database file {0} already exists.  Please remove or use a different filename.\n".format(dbfilename)
-        sys.exit(1)
-    else:
-        dbconn = sqlite3.connect(dbfilename)
-        dbconn.execute("PRAGMA synchronous = OFF")
-        dbconn.execute("PRAGMA temp_store = MEMORY")
-        dbconn.execute("PRAGMA journal_mode = MEMORY")
-else:
-    dbconn = sqlite3.connect(':memory:')
-
-dbcursor = dbconn.cursor()
-
-# Create the table for the ref file data (COLUMNS: locus, base)
-dbcursor.execute('''CREATE TABLE REF_DATA (locus integer PRIMARY KEY ASC, base text NOT NULL)''')
-dbcursor.execute('''CREATE INDEX REF_DATA_LOCUS_BASE_IDX ON REF_DATA (locus,base)''')
-
-# Create the table for the snp file data (COLUMNS: strainid, locus, base)
-dbcursor.execute('''CREATE TABLE SNP_DATA (strainid text, locus integer, base text NOT NULL, PRIMARY KEY(strainid,locus))''')
-
-# Create indexes.
-dbcursor.execute('''CREATE INDEX SNP_DATA_LOCUS_IDX ON SNP_DATA (locus)''')
-dbcursor.execute('''CREATE INDEX SNP_DATA_STRAINID_IDX ON SNP_DATA (strainid)''')
-dbcursor.execute('''CREATE INDEX SNP_DATA_STRAINID_LOCUS_BASE_IDX ON SNP_DATA (strainid,locus,base)''')
-
-dbconn.commit()
-
+refData = {} # This is a dictionary of the ref base. Key is locus, value is base.
 
 print_all("Loading loci entries from reference file...\n")
 # Read in and store reference data in REF_DATA table.
@@ -162,20 +118,18 @@ for line in reffile:
 
     refMatch = refRe.match(line)
     if refMatch:
-        locus = refMatch.group('locus')
+        locus = int(refMatch.group('locus'))
         base = refMatch.group('base')
         # Check for conflict.  Fail if mismatched base at locus, ignore if identical base at locus, insert if no row exists.
-        dbcursor.execute("SELECT base FROM REF_DATA where locus = ?",(locus,))
-        dbresult = dbcursor.fetchone()
-        if dbresult and dbresult[0] != base:
-            print_all("*** ERROR: Duplicate loci found! Read base {} at loci {}, which collides with previous base {} at same loci!  On line {} of reference file.\n".format(base,locus,dbresult[0],lineNumber))
-            print "Failed."
-            sys.exit(1)
-        elif dbresult and dbresult[0] == base:
-            print_debug("Skipping duplicate (but identical) locus/base at line {} for locus {} and base {}\n".format(lineNumber,locus,base))
+        if locus in refData:
+            if refData[locus] != base:
+                print_all("*** ERROR: Duplicate loci found! Read base {} at loci {}, which collides with previous base {} at same loci!  On line {} of reference file.\n".format(base,locus,refData[locus],lineNumber))
+                print "Failed."
+                sys.exit(1)
+            else:
+                print_debug("Skipping duplicate (but identical) locus/base at line {} for locus {} and base {}\n".format(lineNumber,locus,base))
         else:
-            dbconn.execute("INSERT INTO REF_DATA (locus, base) VALUES (?,?)",(locus,base))
-            dbconn.commit()
+            refData[locus] = base
     else:
         print_all("*** ERROR: Reference file format not recognized.  Expected two columns -- [Loci (numeric)] [Base (A-Z)].  Got {} at line {} instead.\n".format(line,lineNumber))
         print "Failed.\n"
@@ -186,55 +140,54 @@ reffile.close()
 print_all("Read {} loci entries from reference file.\n".format(lineNumber))
 
 print_all("Loading loci entries from strain snp data file...\n")
-# Read in and store SNP data in SNP_DATA table.
+# Read in SNP data and store in the snpData dictionary
+# The snpData dictionary has strainIDs as the key, and the values are
+# a dictionary of locus keys and base values for that strain.
 lineNumber=0
 warnings=0
 strainCount = 0
-
+strainNameList = [] # Store strain IDs in a list to preserve order when writing to file later.
 # Expect reference data to be in a three-column format: StrainID Loci Base
 snpRe = re.compile("^(?P<strainid>\S+)\t(?P<locus>\d+)\t(?P<base>[A-Z]+)$")
-with dbconn:
-    currentStrain = ""
-    for line in infile:
-        lineNumber += 1
+currentStrain = ""
+snpData = {}
+for line in infile:
+    lineNumber += 1
 
-        snpMatch = snpRe.match(line)
+    snpMatch = snpRe.match(line)
 
-        # Quit on bad lines.  Bad data!
-        if snpMatch == None:
-            print_all("*** ERROR: Bad data format on line {} of SNP loci file.  Expect three columns [Strain ID (Alphanumeric)] [Loci (Numeric)] [Base (alphabetic)]\nGot {} instead.\n".format(lineNumber,line))
-            print "Failed.\n"
-            sys.exit(1)
+    # Quit on bad lines.  Bad data!
+    if snpMatch == None:
+        print_all("*** ERROR: Bad data format on line {} of SNP loci file.  Expect three columns [Strain ID (Alphanumeric)] [Loci (Numeric)] [Base (alphabetic)]\nGot {} instead.\n".format(lineNumber,line))
+        print "Failed.\n"
+        sys.exit(1)
 
-        strainid = snpMatch.group('strainid')
-        locus = snpMatch.group('locus')
-        base = snpMatch.group('base')
+    strainid = snpMatch.group('strainid')
+    locus = int(snpMatch.group('locus'))
+    base = snpMatch.group('base')
 
-        if currentStrain != strainid:
-            strainCount += 1
-            currentStrain = strainid
+    if currentStrain != strainid:
+        strainCount += 1
+        currentStrain = strainid
+        strainNameList.append(currentStrain)
+        snpData[currentStrain] = {}
 
-        # Sanity checks - These just generate warnings
-        # Check that the locus exists in the reference base.
-        dbcursor.execute("SELECT * from REF_DATA where locus = ?",(locus,))
-        dbresult = dbcursor.fetchone()
-        if not dbresult:
-            print_all("*** WARNING: Encountered SNP loci {}, which does NOT exist in base ref sequence! (line {} of SNP input file) Skipping...\n".format(locus,lineNumber))
-            warnings += 1
-            continue
+    # Sanity checks - These just generate warnings
+    # Check that the locus exists in the reference base.
+    if not locus in refData:
+        print_all("*** WARNING: Encountered SNP loci {}, which does NOT exist in base ref sequence! (line {} of SNP input file) Skipping...\n".format(locus,lineNumber))
+        warnings += 1
+        continue
 
-        # Check that the locus doesn't already exist for this strain
-        dbcursor.execute("SELECT * from SNP_DATA where strainid = ? and locus = ?",(strainid,locus,))
-        dbresult = dbcursor.fetchone()
-        if dbresult:
-            print_all("*** WARNING: Duplicate loci found for same strain id! (line {} of SNP input file, colliding on loci {}) Skipping (will use existing loci)...\n".format(lineNumber,locus))
-            warnings += 1
-            continue
+    # Check that the locus doesn't already exist for this strain
+    if locus in snpData[currentStrain]:
+        print_all("*** WARNING: Duplicate loci found for same strain id! (line {} of SNP input file, colliding on loci {}) Skipping (will use existing loci)...\n".format(lineNumber,locus))
+        warnings += 1
+        continue
 
-
-        # Load into database
-        print_debug("Diag: read in {}, {}, {}\n".format(strainid,locus,base))
-        dbconn.execute("INSERT INTO SNP_DATA (strainid, locus, base) VALUES (?,?,?)",(strainid,locus,base))
+    # Load into SNP dictionary
+    print_debug("Diag: read in {}, {}, {}\n".format(strainid,locus,base))
+    snpData[currentStrain][locus]=base
 
 print
 infile.close()
@@ -244,30 +197,25 @@ seqRecordList = []
 
 # Store the reference base sequence as the first output in the sequence record array
 print_all("Generating reference sequence record.\n")
-dbcursor.execute('''select group_concat(base,"") from REF_DATA order by locus asc;''')
-dbresult = dbcursor.fetchone()
-if dbresult:
-    refSequenceString = dbresult[0]
-    refSeq = Bio.Seq.Seq(refSequenceString,Bio.Alphabet.Alphabet)
-    refSeqRecord = Bio.SeqRecord.SeqRecord(refSeq,id="REF",description="")
-    seqRecordList.append(refSeqRecord)
-else:
-    print_all("*** ERROR: Failed to generate reference sequence record.  No data?\n")
-    sys.exit(1)
+
+refKeys = refData.keys()
+refKeys.sort()
+refSequenceString = "".join([ refData[locus] for locus in refKeys])
+refSeq = Bio.Seq.Seq(refSequenceString,Bio.Alphabet.Alphabet)
+refSeqRecord = Bio.SeqRecord.SeqRecord(refSeq,id="REF",description="")
+seqRecordList.append(refSeqRecord)
     
 # Reconstruct the input strains and store those into the sequence record array
-straincursor = dbconn.cursor()
-straincursor.execute('''select distinct strainid from SNP_DATA order by strainid;''')
-for strainRow in straincursor.fetchall():
-    currentStrainID = strainRow[0]
-    print_all("Now reconstructing strain {}".format(currentStrainID))
+for strainID in strainNameList: 
+    print_all("Now reconstructing strain {}".format(strainID))
+    strainData = refData.copy()
+    strainData.update(snpData[strainID])
 
-    dbcursor.execute('''select group_concat(coalesce(b.base,a.base),"") from REF_DATA a LEFT OUTER JOIN SNP_DATA b on a.locus = b.locus and b.strainid=?  order by a.locus asc;''',(currentStrainID,))
-    dbresult = dbcursor.fetchone()
-
-    snpSequenceString = dbresult[0]
+    strainKeys = strainData.keys()
+    strainKeys.sort()
+    snpSequenceString = "".join([strainData[locus] for locus in strainKeys])
     snpSeq = Bio.Seq.Seq(snpSequenceString,Bio.Alphabet.Alphabet)
-    snpSeqRecord = Bio.SeqRecord.SeqRecord(snpSeq,id=currentStrainID,description="")
+    snpSeqRecord = Bio.SeqRecord.SeqRecord(snpSeq,id=strainID,description="")
     seqRecordList.append(snpSeqRecord)
 
 # Write it all out
