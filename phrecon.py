5e67be66f226b39c0853be6dd3b7ded2c186f7e5
codinghedgehog <github.20.cello@mapspart.com>
2013-08-04 19:44:31 -0500

    Version 4.0 - Added multiprocessing support to phrecon.pl
    Version 3.0 will be the code base for the non-multiprocessing version, as a save point.

--- phrecon.py
+++ phrecon.py
@@ -28,6 +28,7 @@
 #
 # 8/2/2013 - Version 3 - Andrew Pann, Ported version 2.1 of Perl-based Phrecon to Python, using Biopython and Sqlite3.
 # 8/4/2013 - Version 3.1 - Andrew Pann, Reverted to normal port using dictionaries and Biopython (no database).
+# 8/4/2013 - Version 4.0 - Andrew Pann, Added use of multiprocessing.
 
 import argparse
 import os
@@ -38,12 +39,15 @@ import Bio.Seq
 import Bio.SeqIO
 import Bio.SeqRecord
 import Bio.Alphabet
+import multiprocessing
+import pprint
+import signal
 
-VERSION = "3.1"
+VERSION = "4.0"
 
-#############
-# FUNCTIONS
-#############
+#####################
+# UTILITY FUNCTIONS
+#####################
 
 def print_debug(msg):
     '''Prints output to STDOUT and logfile if debug flag is set.
@@ -69,159 +73,231 @@ def print_all(msg):
     logfile.write("{}\n".format(msg))
 
 
+########################################
+# FUNCTIONS RELATED TO MULTIPROCESSING
+########################################
+
+def mp_reconstruct_strain(myStrainID,myStrainData,myRefData):
+    '''This function is used by the multiprocessing feature.
+       It takes in three arguments: strainid name, strain id's
+       dictionary object (locus=>base), the reference base 
+       dictionary data (locus=>base).
+
+       This function returns a two-tuple of the strainid name and
+       the string representing the reconstructed snp sequence,
+       or an Exception object as the second two-tuple value if
+       there is an error.
+    '''
+    try:
+        strainData = myRefData.copy()
+        strainData.update(myStrainData)
+
+        strainKeys = strainData.keys()
+        strainKeys.sort()
+        snpSequenceString = "".join([strainData[locus] for locus in strainKeys])
+    except Exception as error:
+        return (myStrainID,error)
+    else:
+        return (myStrainID,snpSequenceString)
+
+def mp_init_worker():
+    '''Initializer function for Pool. Called by each worker process, and will
+       make them ignore interrupts, to be handled by the main thread.
+    '''
+    signal.signal(signal.SIGINT, signal.SIG_IGN)
+
+def mp_store_result_callback(result):
+    '''Callback function for Pool's async calls.  Stores the result
+       in the result table.
+    '''
+    logFileLock.acquire()
+    if isinstance(result[1],Exception):
+        print_all("*** ERROR: Exception thrown while processing strain {}: {}".format(result[0],pformat(result[1])))
+    else:
+        print_all("Now reconstructing strain {}".format(result[0]))
+        resultTable[result[0]] = result[1]
+    logFileLock.release()
+        
+
 ########################
 # MAIN
 ########################
 
-print "\nPhrecon (Phylo Reconstructor) v{}\n\n".format(VERSION)
 
-# Setup argument parsing
+if __name__ == '__main__':
 
-parser = argparse.ArgumentParser()
-parser.add_argument('reffilename', help="Reference base file name with line format [LOCUS] [BASE]")
-parser.add_argument('snpfilename', help="SNP data file name with line format [STRAIN_ID] [LOCUS] [BASE]")
-parser.add_argument('-debug','--debug', action="store_true", help="Enable debugging output")
-parser.add_argument('-quiet','--quiet', action="store_true", help="Run in quiet mode (suppress most screen output)")
-args = parser.parse_args()
+    print "\nPhrecon (Phylo Reconstructor) v{}\n\n".format(VERSION)
 
-reffile = open(args.reffilename,"r")
-infile = open(args.snpfilename,"r")
-logfilename = "{}.reconstructed.log".format(args.snpfilename)
-logfile = open(logfilename,"w")
+    # Setup argument parsing
 
-#outfile is not needed since BioPython will write to it.  Just need to generate the filename here.
-outfilename = "{}.reconstructed".format(args.snpfilename)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('reffilename', help="Reference base file name with line format [LOCUS] [BASE]")
+    parser.add_argument('snpfilename', help="SNP data file name with line format [STRAIN_ID] [LOCUS] [BASE]")
+    parser.add_argument('-debug','--debug', action="store_true", help="Enable debugging output")
+    parser.add_argument('-quiet','--quiet', action="store_true", help="Run in quiet mode (suppress most screen output)")
+    parser.add_argument('-cpus','--cpus', type=int, help="Indicates how many CPUs to use during multiprocessing.  Default is the detected number of CPUs from Python's multiprocessing module.  Setting this to 1 basically disables multiprocessing.",default=multiprocessing.cpu_count())
+    args = parser.parse_args()
 
-debug = args.debug
-quiet = args.quiet
+    reffile = open(args.reffilename,"r")
+    infile = open(args.snpfilename,"r")
+    logfilename = "{}.reconstructed.log".format(args.snpfilename)
+    logfile = open(logfilename,"w")
 
-if debug:
-    print "Producing debug output.\n"
+    #outfile is not needed since BioPython will write to it.  Just need to generate the filename here.
+    outfilename = "{}.reconstructed".format(args.snpfilename)
 
-if quiet:
-    print "Producing quiet (no stdout) output.  Logfile is still generated.\n"
+    debug = args.debug
+    quiet = args.quiet
+
+    cpu_num=args.cpus
+
+    if debug:
+        print "Producing debug output.\n"
+
+    if quiet:
+        print "Producing quiet (no stdout) output.  Logfile is still generated.\n"
+
+    print_all("CPUs to use: {}".format(cpu_num))
+
+    refData = {} # This is a dictionary of the ref base. Key is locus, value is base.
+
+    print_all("Loading loci entries from reference file...")
+    # Read in and store reference data in REF_DATA table.
+    lineNumber=0
+
+    # Expect reference data to be in a two-column format: Loci Base
+    refRe = re.compile("^(?P<locus>\d+)\s+(?P<base>[A-Z]+)$")
+    for line in reffile:
+        lineNumber += 1
 
 
-refData = {} # This is a dictionary of the ref base. Key is locus, value is base.
-
-print_all("Loading loci entries from reference file...\n")
-# Read in and store reference data in REF_DATA table.
-lineNumber=0
-
-# Expect reference data to be in a two-column format: Loci Base
-refRe = re.compile("^(?P<locus>\d+)\s+(?P<base>[A-Z]+)$")
-for line in reffile:
-    lineNumber += 1
-
-
-    refMatch = refRe.match(line)
-    if refMatch:
-        locus = int(refMatch.group('locus'))
-        base = refMatch.group('base')
-        # Check for conflict.  Fail if mismatched base at locus, ignore if identical base at locus, insert if no row exists.
-        if locus in refData:
-            if refData[locus] != base:
-                print_all("*** ERROR: Duplicate loci found! Read base {} at loci {}, which collides with previous base {} at same loci!  On line {} of reference file.\n".format(base,locus,refData[locus],lineNumber))
-                print "Failed."
-                sys.exit(1)
+        refMatch = refRe.match(line)
+        if refMatch:
+            locus = int(refMatch.group('locus'))
+            base = refMatch.group('base')
+            # Check for conflict.  Fail if mismatched base at locus, ignore if identical base at locus, insert if no row exists.
+            if locus in refData:
+                if refData[locus] != base:
+                    print_all("*** ERROR: Duplicate loci found! Read base {} at loci {}, which collides with previous base {} at same loci!  On line {} of reference file.".format(base,locus,refData[locus],lineNumber))
+                    print "Failed."
+                    sys.exit(1)
+                else:
+                    print_debug("Skipping duplicate (but identical) locus/base at line {} for locus {} and base {}\n".format(lineNumber,locus,base))
             else:
-                print_debug("Skipping duplicate (but identical) locus/base at line {} for locus {} and base {}\n".format(lineNumber,locus,base))
+                refData[locus] = base
         else:
-            refData[locus] = base
+            print_all("*** ERROR: Reference file format not recognized.  Expected two columns -- [Loci (numeric)] [Base (A-Z)].  Got {} at line {} instead.".format(line,lineNumber))
+            print "Failed.\n"
+            sys.exit(1)
+
+    print
+    reffile.close()
+    print_all("Read {} loci entries from reference file.".format(lineNumber))
+
+    print_all("Loading loci entries from strain snp data file...")
+    # Read in SNP data and store in the snpData dictionary
+    # The snpData dictionary has strainIDs as the key, and the values are
+    # a dictionary of locus keys and base values for that strain.
+    lineNumber=0
+    warnings=0
+    strainCount = 0
+    strainNameList = [] # Store strain IDs in a list to preserve order when writing to file later.
+    # Expect snp data file lines to be in a three-column format: StrainID Loci Base
+    snpRe = re.compile("^(?P<strainid>\S+)\t(?P<locus>\d+)\t(?P<base>[A-Z]+)$")
+    currentStrain = ""
+    snpData = {}
+    for line in infile:
+        lineNumber += 1
+
+        snpMatch = snpRe.match(line)
+
+        # Quit on bad lines.  Bad data!
+        if snpMatch == None:
+            print_all("*** ERROR: Bad data format on line {} of SNP loci file.  Expect three columns [Strain ID (Alphanumeric)] [Loci (Numeric)] [Base (alphabetic)]\nGot {} instead.".format(lineNumber,line))
+            print "Failed.\n"
+            sys.exit(1)
+
+        strainid = snpMatch.group('strainid')
+        locus = int(snpMatch.group('locus'))
+        base = snpMatch.group('base')
+
+        if currentStrain != strainid:
+            strainCount += 1
+            currentStrain = strainid
+            strainNameList.append(currentStrain)
+            snpData[currentStrain] = {}
+
+        # Sanity checks - These just generate warnings
+        # Check that the locus exists in the reference base.
+        if not locus in refData:
+            print_all("*** WARNING: Encountered SNP loci {}, which does NOT exist in base ref sequence! (line {} of SNP input file) Skipping...".format(locus,lineNumber))
+            warnings += 1
+            continue
+
+        # Check that the locus doesn't already exist for this strain
+        if locus in snpData[currentStrain]:
+            print_all("*** WARNING: Duplicate loci found for same strain id! (line {} of SNP input file, colliding on loci {}) Skipping (will use existing loci)...".format(lineNumber,locus))
+            warnings += 1
+            continue
+
+        # Load into SNP dictionary
+        print_debug("Diag: read in {}, {}, {}\n".format(strainid,locus,base))
+        snpData[currentStrain][locus]=base
+
+    print
+    infile.close()
+    print_all("Read {} loci entries from snp data file.".format(lineNumber))
+
+    seqRecordList = []
+
+    # Store the reference base sequence as the first output in the sequence record array
+    print_all("Generating reference sequence record.")
+
+    refKeys = refData.keys()
+    refKeys.sort()
+    refSequenceString = "".join([ refData[locus] for locus in refKeys])
+    refSeq = Bio.Seq.Seq(refSequenceString,Bio.Alphabet.Alphabet)
+    refSeqRecord = Bio.SeqRecord.SeqRecord(refSeq,id="REF",description="")
+    seqRecordList.append(refSeqRecord)
+        
+    ##################
+    # MULTIPROCESSING -- Reconstruct the input strains in parllel and store those into the sequence record array.
+    ##################
+
+    # Setup a lock -- this is just to neatly interleave output to the log file (otherwise missing or garbles lines can occur).
+    logFileLock = multiprocessing.Lock()
+    resultTable = {} # Dictionary of strainID => base sequence string.  Filled by multiprocesing call.
+    mpPool = multiprocessing.Pool(processes=cpu_num,initializer=mp_init_worker)
+    print_all("Submitting strain reconstruction requests to pool...")
+    try:
+        for strainID in strainNameList: 
+            mpPool.apply_async(mp_reconstruct_strain,args=(strainID,snpData[strainID],refData),callback=mp_store_result_callback)
+
+    except KeyboardInterrupt:
+        print "Aborting..."
+        mpPool.terminate()
+        mpPool.join()
     else:
-        print_all("*** ERROR: Reference file format not recognized.  Expected two columns -- [Loci (numeric)] [Base (A-Z)].  Got {} at line {} instead.\n".format(line,lineNumber))
-        print "Failed.\n"
-        sys.exit(1)
+        print_all("Waiting for working processes to return...")
+        mpPool.close()
+        mpPool.join()
 
-print
-reffile.close()
-print_all("Read {} loci entries from reference file.\n".format(lineNumber))
+    # Process results from multiprocessing.
+    # Namely this means generating the list of SeqRecords (in same order as the snp input file)
+    # to write to the output file with Biopython.
+    print_all("Processing results...")
+    for strainID in strainNameList:
+        snpSequenceString = resultTable[strainID]
+        snpSeq = Bio.Seq.Seq(snpSequenceString,Bio.Alphabet.Alphabet)
+        snpSeqRecord = Bio.SeqRecord.SeqRecord(snpSeq,id=strainID,description="")
+        seqRecordList.append(snpSeqRecord)
 
-print_all("Loading loci entries from strain snp data file...\n")
-# Read in SNP data and store in the snpData dictionary
-# The snpData dictionary has strainIDs as the key, and the values are
-# a dictionary of locus keys and base values for that strain.
-lineNumber=0
-warnings=0
-strainCount = 0
-strainNameList = [] # Store strain IDs in a list to preserve order when writing to file later.
-# Expect reference data to be in a three-column format: StrainID Loci Base
-snpRe = re.compile("^(?P<strainid>\S+)\t(?P<locus>\d+)\t(?P<base>[A-Z]+)$")
-currentStrain = ""
-snpData = {}
-for line in infile:
-    lineNumber += 1
+    # Write it all out.
+    print_all("Writing output file...")
+    Bio.SeqIO.write(seqRecordList,outfilename,"fasta")
 
-    snpMatch = snpRe.match(line)
-
-    # Quit on bad lines.  Bad data!
-    if snpMatch == None:
-        print_all("*** ERROR: Bad data format on line {} of SNP loci file.  Expect three columns [Strain ID (Alphanumeric)] [Loci (Numeric)] [Base (alphabetic)]\nGot {} instead.\n".format(lineNumber,line))
-        print "Failed.\n"
-        sys.exit(1)
-
-    strainid = snpMatch.group('strainid')
-    locus = int(snpMatch.group('locus'))
-    base = snpMatch.group('base')
-
-    if currentStrain != strainid:
-        strainCount += 1
-        currentStrain = strainid
-        strainNameList.append(currentStrain)
-        snpData[currentStrain] = {}
-
-    # Sanity checks - These just generate warnings
-    # Check that the locus exists in the reference base.
-    if not locus in refData:
-        print_all("*** WARNING: Encountered SNP loci {}, which does NOT exist in base ref sequence! (line {} of SNP input file) Skipping...\n".format(locus,lineNumber))
-        warnings += 1
-        continue
-
-    # Check that the locus doesn't already exist for this strain
-    if locus in snpData[currentStrain]:
-        print_all("*** WARNING: Duplicate loci found for same strain id! (line {} of SNP input file, colliding on loci {}) Skipping (will use existing loci)...\n".format(lineNumber,locus))
-        warnings += 1
-        continue
-
-    # Load into SNP dictionary
-    print_debug("Diag: read in {}, {}, {}\n".format(strainid,locus,base))
-    snpData[currentStrain][locus]=base
-
-print
-infile.close()
-print_all("Read {} loci entries from snp data file.\n".format(lineNumber))
-
-seqRecordList = []
-
-# Store the reference base sequence as the first output in the sequence record array
-print_all("Generating reference sequence record.\n")
-
-refKeys = refData.keys()
-refKeys.sort()
-refSequenceString = "".join([ refData[locus] for locus in refKeys])
-refSeq = Bio.Seq.Seq(refSequenceString,Bio.Alphabet.Alphabet)
-refSeqRecord = Bio.SeqRecord.SeqRecord(refSeq,id="REF",description="")
-seqRecordList.append(refSeqRecord)
-    
-# Reconstruct the input strains and store those into the sequence record array
-for strainID in strainNameList: 
-    print_all("Now reconstructing strain {}".format(strainID))
-    strainData = refData.copy()
-    strainData.update(snpData[strainID])
-
-    strainKeys = strainData.keys()
-    strainKeys.sort()
-    snpSequenceString = "".join([strainData[locus] for locus in strainKeys])
-    snpSeq = Bio.Seq.Seq(snpSequenceString,Bio.Alphabet.Alphabet)
-    snpSeqRecord = Bio.SeqRecord.SeqRecord(snpSeq,id=strainID,description="")
-    seqRecordList.append(snpSeqRecord)
-
-# Write it all out
-print_all("Writing output file...\n")
-Bio.SeqIO.write(seqRecordList,outfilename,"fasta")
-
-print_all("\n{} strains processed from SNP loci input file.\n".format(strainCount));
-print_all("\n{} warnings were detected.\n".format(warnings));
-print "Log file for this run can be found in {}\n".format(logfilename)
+    print_all("\n{} strains processed from SNP loci input file.".format(strainCount));
+    print_all("\n{} warnings were detected.".format(warnings));
+    print "Log file for this run can be found in {}\n".format(logfilename)
 
 
